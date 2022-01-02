c6564e8112ef9a6588974ed9443cb201865d49d5
wuliao <1182300246@qq.com>
2022-01-03 00:30:39 +0800

    使用宏定义

--- lab_4/rtl/aludec.v
+++ lab_4/rtl/aludec.v
@@ -19,7 +19,7 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
+`include "defines.vh"
 module aludec(
 	input wire[5:0] funct,
 	input wire[1:0] aluop,
@@ -30,11 +30,11 @@ module aludec(
 			2'b00: alucontrol <= 3'b010;//add (for lw/sw/addi)
 			2'b01: alucontrol <= 3'b110;//sub (for beq)
 			default : case (funct)
-				6'b100000:alucontrol <= 3'b010; //add
-				6'b100010:alucontrol <= 3'b110; //sub
-				6'b100100:alucontrol <= 3'b000; //and
-				6'b100101:alucontrol <= 3'b001; //or
-				6'b101010:alucontrol <= 3'b111; //slt
+				`EXE_ADD:alucontrol <= 3'b010; //add
+				`EXE_SUB:alucontrol <= 3'b110; //sub
+				`EXE_AND:alucontrol <= 3'b000; //and
+				`EXE_OR:alucontrol <= 3'b001; //or
+				`EXE_SLT:alucontrol <= 3'b111; //slt
 				default:  alucontrol <= 3'b000;
 			endcase
 		endcase
