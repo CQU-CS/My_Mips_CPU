36e466494e502c6c6d43c38ced03a02a782adebe
卓钊 <18350120264@163.com>
2022-01-02 16:05:40 +0800

    读取数据指令

--- lab_4/rtl/controller.v
+++ lab_4/rtl/controller.v
@@ -1,82 +1,102 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
+// Company:
+// Engineer:
+//
 // Create Date: 2017/10/23 15:21:30
-// Design Name: 
+// Design Name:
 // Module Name: controller
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
+// Project Name:
+// Target Devices:
+// Tool Versions:
+// Description:
+//
+// Dependencies:
+//
 // Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
-// 
+//
 //////////////////////////////////////////////////////////////////////////////////
 
 
 module controller(
-	input wire clk,rst,
-	//decode stage
-	input wire[5:0] opD,functD,
-	output wire pcsrcD,branchD,equalD,jumpD,
-	
-	//execute stage
-	input wire flushE,
-	output wire memtoregE,alusrcE,
-	output wire regdstE,regwriteE,	
-	output wire[2:0] alucontrolE,
+           input wire clk,rst,
+           //decode stage
+           input wire[5:0] opD,functD,
+           output wire pcsrcD,branchD,equalD,jumpD,
 
-	//mem stage
-	output wire memtoregM,memwriteM,
-				regwriteM,
-	//write back stage
-	output wire memtoregW,regwriteW
+           //execute stage
+           input wire flushE,
+           output wire memtoregE,alusrcE,
+           output wire regdstE,regwriteE,
+           output wire[2:0] alucontrolE,
 
-    );
-	
-	//decode stage
-	wire[1:0] aluopD;
-	wire memtoregD,memwriteD,alusrcD,
-		regdstD,regwriteD;
-	wire[2:0] alucontrolD;
+           //mem stage
+           output wire memtoregM,memwriteM,
+           regwriteM,
+           output wire[2:0] lshbM,
+           //write back stage
+           output wire memtoregW,regwriteW,
+           output wire[2:0] lshbW
 
-	//execute stage
-	wire memwriteE;
+       );
 
-	maindec md(
-		opD,
-		memtoregD,memwriteD,
-		branchD,alusrcD,
-		regdstD,regwriteD,
-		jumpD,
-		aluopD
-		);
-	aludec ad(functD,aluopD,alucontrolD);
+//decode stage
+wire[1:0] aluopD;
+wire memtoregD,memwriteD,alusrcD,
+     regdstD,regwriteD;
+wire[2:0] alucontrolD;
 
-	assign pcsrcD = branchD & equalD;
+//execute stage
+wire memwriteE;
 
-	//pipeline registers
-	floprc #(8) regE(
-		clk,
-		rst,
-		flushE,
-		{memtoregD,memwriteD,alusrcD,regdstD,regwriteD,alucontrolD},
-		{memtoregE,memwriteE,alusrcE,regdstE,regwriteE,alucontrolE}
-		);
-	flopr #(8) regM(
-		clk,rst,
-		{memtoregE,memwriteE,regwriteE},
-		{memtoregM,memwriteM,regwriteM}
-		);
-	flopr #(8) regW(
-		clk,rst,
-		{memtoregM,regwriteM},
-		{memtoregW,regwriteW}
-		);
+maindec md(
+            opD,
+            memtoregD,memwriteD,
+            branchD,alusrcD,
+            regdstD,regwriteD,
+            jumpD,
+            aluopD,
+            lshbD
+        );
+aludec ad(functD,aluopD,alucontrolD);
+
+assign pcsrcD = branchD & equalD;
+
+//pipeline registers
+floprc #(8) regE(
+           clk,
+           rst,
+           flushE,
+           {memtoregD,memwriteD,alusrcD,regdstD,regwriteD,alucontrolD},
+           {memtoregE,memwriteE,alusrcE,regdstE,regwriteE,alucontrolE}
+       );
+floprc #(3) regE2(
+           clk,
+           rst,
+           flushE,
+           lshbD,
+           lshbE
+       );
+flopr #(8) regM(
+          clk,rst,
+          {memtoregE,memwriteE,regwriteE},
+          {memtoregM,memwriteM,regwriteM}
+      );
+flopr #(3) regM2(
+          clk,rst,
+          lshbE,
+          lshbM
+      );
+flopr #(8) regW(
+          clk,rst,
+          {memtoregM,regwriteM},
+          {memtoregW,regwriteW}
+      );
+flopr #(3) regW2(
+          clk,rst,
+          lshbM,
+          lshbW
+      );
 endmodule
--- lab_4/rtl/mips.v
+++ lab_4/rtl/mips.v
@@ -1,82 +1,86 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
+// Company:
+// Engineer:
+//
 // Create Date: 2017/11/07 10:58:03
-// Design Name: 
+// Design Name:
 // Module Name: mips
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
+// Project Name:
+// Target Devices:
+// Tool Versions:
+// Description:
+//
+// Dependencies:
+//
 // Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
-// 
+//
 //////////////////////////////////////////////////////////////////////////////////
 
 
 module mips(
-	input wire clk,rst,
-	output wire[31:0] pcF,
-	input wire[31:0] instrF,
-	output wire memwriteM,
-	output wire[31:0] aluoutM,writedataM,
-	input wire[31:0] readdataM 
-    );
-	
-	wire [5:0] opD,functD;
-	wire regdstE,alusrcE,pcsrcD,memtoregE,memtoregM,memtoregW,
-			regwriteE,regwriteM,regwriteW;
-	wire [2:0] alucontrolE;
-	wire flushE,equalD;
+           input wire clk,rst,
+           output wire[31:0] pcF,
+           input wire[31:0] instrF,
+           output wire memwriteM,
+           output wire[31:0] aluoutM,writedataM,
+           input wire[31:0] readdataM
+       );
 
-	controller c(
-		clk,rst,
-		//decode stage
-		opD,functD,
-		pcsrcD,branchD,equalD,jumpD,
-		
-		//execute stage
-		flushE,
-		memtoregE,alusrcE,
-		regdstE,regwriteE,	
-		alucontrolE,
+wire [5:0] opD,functD;
+wire regdstE,alusrcE,pcsrcD,memtoregE,memtoregM,memtoregW,
+     regwriteE,regwriteM,regwriteW;
+//访存信号
+wire [2:0] lshbM;
+wire [2:0] lshbW;
 
-		//mem stage
-		memtoregM,memwriteM,
-		regwriteM,
-		//write back stage
-		memtoregW,regwriteW
-		);
-	datapath dp(
-		clk,rst,
-		//fetch stage
-		pcF,
-		instrF,
-		//decode stage
-		pcsrcD,branchD,
-		jumpD,
-		equalD,
-		opD,functD,
-		//execute stage
-		memtoregE,
-		alusrcE,regdstE,
-		regwriteE,
-		alucontrolE,
-		flushE,
-		//mem stage
-		memtoregM,
-		regwriteM,
-		aluoutM,writedataM,
-		readdataM,
-		//writeback stage
-		memtoregW,
-		regwriteW
-	    );
-	
+wire [2:0] alucontrolE;
+wire flushE,equalD;
+
+controller c(
+               clk,rst,
+               //decode stage
+               opD,functD,
+               pcsrcD,branchD,equalD,jumpD,
+
+               //execute stage
+               flushE,
+               memtoregE,alusrcE,
+               regdstE,regwriteE,
+               alucontrolE,
+
+               //mem stage
+               memtoregM,memwriteM,
+               regwriteM,lshbM,
+               //write back stage
+               memtoregW,regwriteW,lshbW
+           );
+datapath dp(
+             clk,rst,
+             //fetch stage
+             pcF,
+             instrF,
+             //decode stage
+             pcsrcD,branchD,
+             jumpD,
+             equalD,
+             opD,functD,
+             //execute stage
+             memtoregE,
+             alusrcE,regdstE,
+             regwriteE,
+             alucontrolE,
+             flushE,
+             //mem stage
+             memtoregM,
+             regwriteM,
+             aluoutM,writedataM,
+             readdataM,lshbM,
+             //writeback stage
+             memtoregW,
+             regwriteW,lshbW
+         );
+
 endmodule
